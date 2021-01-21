7dfecb1ec7286aeae28839852a0ff58e639c242a
EricNavar <ericnavar@ufl.edu>
2021-01-20 19:48:34 -0500

    use NoSsr component - enclose AboutUs, OfficerBanner, and ContactBanner in a NoSsr component remove CircularProgress

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, lazy, Suspense } from "react";
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import HomeBanner from "../Components/HomeBanner";
+import NoSsr from '@material-ui/core/NoSsr';
 const AboutUs = lazy(() => import("../Components/AboutUs Section/AboutUs"));
 const OfficersBanner = lazy(() => import("../Components/OfficersBanner Section/OfficersBanner"));
 const ContactBanner = lazy(() => import("../Components/Contact/ContactBanner"));
@@ -30,15 +30,13 @@ export default function HomePage() {
   return (
     <main className={classes.homePage}>
       <HomeBanner />
-      <Suspense fallback={<CircularProgress/>}>
-        <AboutUs />
-      </Suspense>
-      <Suspense fallback={<CircularProgress/>}>
-        <OfficersBanner />
-      </Suspense>
-      <Suspense fallback={<CircularProgress/>}>
-        <ContactBanner />
-      </Suspense>
+      <NoSsr defer>
+        <Suspense fallback={<div/>}>
+          <AboutUs />
+          <OfficersBanner />
+          <ContactBanner />
+        </Suspense>
+      </NoSsr>
     </main>
   );
 }
