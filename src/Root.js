13d92ac7f54f629bb26aeec75e907ca7ebc49c7a
EricNavar <ericnavar@ufl.edu>
2021-01-20 22:48:50 -0500

    put Error404Page in a Suspense and NoSsr component

--- src/Root.js
+++ src/Root.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { lazy, Suspense }  from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import NoSsr from '@material-ui/core/NoSsr';
 import HomePage from "./screens/HomePage";
-import Error404Page from "./screens/Error404Page";
+const Error404Page = lazy(() => import("./screens/Error404Page"));
 
 //https://reacttraining.com/react-router/web/guides/quick-start
 
@@ -56,7 +57,11 @@ export default function Root() {
       <ThemeProvider theme={myTheme}>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route component={Error404Page} />
+          <NoSsr defer>
+            <Suspense fallback={<div/>}>
+              <Route component={Error404Page} />
+            </Suspense>
+          </NoSsr>
         </Switch>
       </ThemeProvider>
     </BrowserRouter>
